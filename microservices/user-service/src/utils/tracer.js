14138c8e4d5e6e3e41c088a07dc2c804bf5ddcaa
Andriy Kobetiak <86183007+Andr1yK@users.noreply.github.com>
2025-03-17 23:42:57 +0200

    [Tracing]: integrate tempo for user service (#23)
    Demo:
    
    <img width="1512" alt="image"
    src="https://github.com/user-attachments/assets/d353a5d7-7f8e-45da-be3d-79d1aa7fe009"
    />

---
+++ microservices/user-service/src/utils/tracer.js
@@ -0,0 +1,52 @@
+const process = require('process');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { Resource } = require('@opentelemetry/resources');
+const { ATTR_SERVICE_NAME, ATTR_DEPLOYMENT_ENVIRONMENT } = require('@opentelemetry/semantic-conventions');
+
+const serviceName = process.env.SERVICE_NAME || 'unknown-service';
+
+const traceExporter = new OTLPTraceExporter({
+  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://tempo:4318/v1/traces',
+});
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [ATTR_SERVICE_NAME]: serviceName,
+    [ATTR_DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV || 'development',
+  }),
+  traceExporter,
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': { enabled: false },
+      '@opentelemetry/instrumentation-express': {
+        enabled: true,
+        ignoreLayersPattern: ['^/metrics', '^/health'],
+      },
+      '@opentelemetry/instrumentation-http': { enabled: true },
+      '@opentelemetry/instrumentation-pg': { enabled: true },
+      '@opentelemetry/instrumentation-pg-pool': { enabled: true },
+      // '@opentelemetry/instrumentation-winston': { enabled: true },
+    }),
+  ],
+});
+
+const initTracer = () => {
+  try {
+    sdk.start();
+
+    console.log('Tracing initialized');
+
+    process.on('SIGTERM', () => {
+      sdk.shutdown()
+        .then(() => console.log('Tracing terminated'))
+        .catch((error) => console.log('Error terminating tracing', error))
+        .finally(() => process.exit(0));
+    });
+  } catch (error) {
+    console.log('Error initializing tracing', error);
+  }
+};
+
+module.exports = { initTracer };
